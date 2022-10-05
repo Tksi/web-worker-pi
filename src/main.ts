6bddf0c62c8b809b2afad51f20fc2a1e90417ab7
Tksi <24800668+Tksi@users.noreply.github.com>
2022-10-05 23:06:36 +0900

    [add] multi thread

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,32 @@
-import './style.css';
 console.time('Timer');
-const worker = new Worker(new URL('./worker.ts', import.meta.url));
 
 const epoch = 1e8;
+const thread = navigator.hardwareConcurrency;
+if (epoch % thread) console.warn('epoch not divisible by thread');
 
-worker.postMessage({ epoch: epoch });
+const workers = Array(thread)
+  .fill('')
+  .map(() => new Worker(new URL('./worker.ts', import.meta.url)));
 
-const inCircles = new Promise((resolve) => {
-  worker.addEventListener('message', ({ data: { inCircles } }) => {
-    resolve(inCircles);
-  });
-});
+for (const worker of workers) {
+  worker.postMessage({ epoch: (epoch / thread) | 0 });
+}
 
-console.log(await inCircles);
+const inCircles = (
+  await Promise.all(
+    workers.map(
+      (worker) =>
+        new Promise((resolve) => {
+          worker.addEventListener('message', ({ data: { inCircles } }) => {
+            resolve(inCircles);
+          });
+        }) as Promise<number>
+    )
+  )
+).reduce((a, b) => a + b);
+
+console.log((inCircles * 4) / epoch);
+
 console.timeEnd('Timer');
+
+export {};
